dfefd908155c340c976e7916b4e971bfde57b575
Jon Riley <jprly@outlook.com>
2021-10-29 22:04:15 -0500

    tech animation script added

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 console.log("connected");
 const techItems = document.querySelector("#firstobserver");
+const allTechItems = document.querySelectorAll(".techicon");
 console.log(techItems);
 
-const callback = () => {
-  console.log("visible");
+const callback = (entries, observer) => {
+  //   console.log("visible", entries, observer);
+  if (entries[0].isIntersecting) {
+    console.log("it is actually visible");
+    // techItems.classList.add("techanimate");
+    // allTechItems.forEach((item) => {
+    //   setTimeout(() => {
+    //     item.classList.add("techanimate");
+    //   }, 1000);
+    // });
+    // for (let i = 0; i < allTechItems.length; i++) {
+    //   console.log(allTechItems[i]);
+    // }
+    let i = 0;
+    const startAnimation = () => {
+      setTimeout(() => {
+        if (i < allTechItems.length) {
+          console.log(i);
+          allTechItems[i].classList.add("techanimate");
+          startAnimation();
+          i++;
+        }
+        // console.log(i);
+      }, 300);
+    };
+    startAnimation();
+  }
+  if (!entries[0].isIntersecting) {
+    allTechItems.forEach((item) => {
+      item.classList.remove("techanimate");
+    });
+  }
 };
 
 let options = {
