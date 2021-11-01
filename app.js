4553f9ca7e07d512aa4251ba16a5f6b2fbd9c2cd
Jon Riley <jprly@outlook.com>
2021-11-01 09:36:34 -0500

    animation update

--- app.js
+++ app.js
@@ -1,43 +1,46 @@
-console.log("connected");
 const techItems = document.querySelector("#firstobserver");
 const allTechItems = document.querySelectorAll(".techicon");
-console.log(techItems);
+
 let i = 0;
+let annimationActive = false;
+
+const startAnimation = () => {
+  setTimeout(() => {
+    if (i < allTechItems.length) {
+      allTechItems[i].classList.add("techanimate");
+      i++;
+
+      startAnimation();
+    }
+  }, 300);
+};
 
 const callback = (entries, observer) => {
-  if (entries[0].isIntersecting) {
+  console.log("ovserver called");
+  if (entries[0].isIntersecting && !annimationActive) {
+    i = 0;
+    annimationActive = true;
     console.log("now visible");
-    console.log(entries);
-    const startAnimation = () => {
-      setTimeout(() => {
-        if (i < allTechItems.length) {
-          allTechItems[i].classList.add("techanimate");
-          startAnimation();
-          i++;
-        }
-      }, 300);
-    };
+    console.log(annimationActive);
+
     startAnimation();
+    annimationActive = false;
+    return;
   }
   if (!entries[0].isIntersecting) {
     i = 0;
     allTechItems.forEach((item) => {
       item.classList.remove("techanimate");
     });
+    return;
   }
 };
 
 let options = {
   root: null,
-
   rootMargin: "0px",
   threshold: 1.0,
 };
 
 let observer = new IntersectionObserver(callback, options);
 observer.observe(techItems);
-
-const portButton = document.querySelector("#portfolio-button");
-portButton.addEventListener("mouseenter", (e) => {
-  console.log("enter", e);
-});
